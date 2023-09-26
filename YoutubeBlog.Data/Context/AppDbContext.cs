79d772ccaaeb126a3208b9132d8e9c8a2afebcaa
Ardahan <ardahan@gmail.com>
2023-09-26 05:05:17 +0300

    initial commit

---
+++ YoutubeBlog.Data/Context/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeBlog.Entity.Entities;
+
+namespace YoutubeBlog.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; } 
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Image> Images { get; set; } 
+
+       
+    }
+}
